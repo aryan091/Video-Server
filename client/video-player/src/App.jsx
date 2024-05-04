1edf73b3d9538f6c7756acd1575b1a92bb391612
Aryan Daftari <aryandaftari7@gmail.com>
2024-05-05 01:17:57 +0530

    Video Stremer

---
+++ client/video-player/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import videojs from 'video.js'
+import { useRef } from 'react'
+
+function App() {
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/courses/9d8ef240-9603-4a8c-a955-06a84c445d07/index.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
